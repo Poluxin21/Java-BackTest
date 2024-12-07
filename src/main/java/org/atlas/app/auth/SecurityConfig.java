a88c8c1dbb4308211f4a5e699255ad025e4db8bf
Guilherme bento da Silva <guilhermebentodasilva55@gmail.com>
2024-12-07 19:31:30 -0300

    first commit

---
+++ src/main/java/org/atlas/app/auth/SecurityConfig.java
@@ -0,0 +1,27 @@
+package org.atlas.app.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@SuppressWarnings("deprecation")
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .requestMatchers("/api/auth/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable();
+
+        return http.build();
+    }
+}
